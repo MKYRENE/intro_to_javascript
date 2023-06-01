a8ef2f96b0649c0f293ffc2be528e101aab38449
MKYRENE <rodriguezm90@icloud.com>
2023-05-31 20:15:33 -0400

    made changes to code on main branch

---
+++ js/script.js
@@ -0,0 +1,31 @@
+// console.log('test');
+var students = ['John', 'FSF', 'Jamil', 'FSF', 'Roman', 'FSF'];
+
+
+
+// for (var index = 0; index < students.length; index++) {
+// index += 1;
+
+// console.log('Name:', students[index]);
+// console.log('Course:', students[index + 1]);
+// }
+
+// index += 1;
+
+function printStudentInfo(studentName, studentCourse) {
+    console.log('Welcome to the class');
+    console.log('Name:', studentName);
+    console.log('Course:', studentCourse);
+    console.log('Please utilize office hours for help!');
+}
+
+// printStudentInfo(students[0], students[1]);
+
+function printStuff(name, age) {
+    console.log(name);
+
+}
+
+printStuff('JD', 43);
+
+// The homework for tonight use the function and call it in our for loop 
